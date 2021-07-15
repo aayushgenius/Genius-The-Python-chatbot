aa449f0ce5cfa08de0b78dc52fe9dcc3aa503c1e
aayushgenius <69508057+aayushgenius@users.noreply.github.com>
2021-07-15 21:57:59 +0530

    Update maincode.py

--- chatbot/code/maincode.py
+++ chatbot/code/maincode.py
@@ -4,11 +4,31 @@ lol = 1
 text = str(input())
 while lol == 1:
     if text == "hi":
-        print("hello")
+        print("hello. My name is genius. Call my name if you need anything.")
         text = str(input())
-    elif text == "hello":
-        print("hi")
+    elif text == "hello, type who to know who I am":
+        print("hi, type 'who' to know who I am")
+    elif text == "who":
+        print("I am a chatbot which is made by aayushgenius. My name is genius.")
         text = str(input())
+    elif text == "genius":
+        print("Hello there, My name is genius. What would you like to tell me? P.S. If you want me to guess your name say guess name. By the way would you like to guess my favourite colour. Say 'guess colour' to do so.")
+        text = str(input())
+    elif text == "guess name":
+        print("I am not the best at guessing names but, is your name Muhammed. If I am wrong then say 'guess name wrong'")
+        text = str(input())
+    elif text == "guess name wrong":
+        print("I got your name wrong. I can't believe it.")
+        name = str(input("By the way, what is your name?"))
+        print(str(name) + " is your name. I was about to guess that. By the way would you like to guess my favourite colour. Say 'guess colour' to do so.")
+        text = str(input())
+    elif text == "guess colour":
+        print("So you want to guess my favourite colour. Lets see how good you do.")
+        colour = str(input())
+        if colour = green:
+            print("You got lucky this time. You guessed my favourite colour. It is green.")
+        else:
+            print("Sorry, thats not my favourite colour. It is green.")
     elif text == "stop":
         break
     else:
